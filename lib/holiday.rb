79bccb6dd3d49f92142d1f46951cd23be02b61c0
Anthony Ybarra <anthony_jy72@yahoo.com>
2019-12-22 19:48:40 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -21,13 +21,12 @@ end
 
 add_supply_to_memorial_day(holiday_supplies, "baloons")
 
-def add_new_season_and_holiday_with_supplies(holiday_hash, season, holiday_name, supply_array)
-  holiday_hash[season] = {holiday_name => supply_array} 
+def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_array)
+  holiday_hash[season][holiday_name] = supply_array
   holiday_hash
 end
-
-mitzvos = ["seder", "matzah", "hagaddah"]
-add_new_season_and_holiday_with_supplies(holiday_supplies, :aviv, :pesach, mitzvos)
+add_new_holiday_with_supplies(holiday_supplies, :fall, :succas, ["lulav", "esrog", "schach"]
+end
 
 def all_winter_holiday_supplies(holiday_hash)
   holiday_hash[:winter].values.flatten
